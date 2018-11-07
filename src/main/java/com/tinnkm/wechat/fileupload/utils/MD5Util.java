372788f03ccc140780402763b510f98643953547
tinnkm <tinnkms@gmail.com>
2018-11-08 07:43:17 +0800

    add mongodb

---
+++ src/main/java/com/tinnkm/wechat/fileupload/utils/MD5Util.java
@@ -0,0 +1,11 @@
+package com.tinnkm.wechat.fileupload.utils;
+
+/**
+ * @author tinnkm
+ * @version 1.0
+ * @classname MD5Util
+ * @description TODO
+ * @date 2018/11/5 21:05
+ **/
+public class MD5Util {
+}
